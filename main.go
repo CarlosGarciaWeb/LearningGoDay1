3b40ed3cf3fa713e142c2d15125a9470e5d86994
Carlos Garcia <carlos.grh@outlook.com>
2022-10-11 07:04:11 -0500

    declare variable int. Int has several types depending on the bit processor being used. You can use int by itself without specifying though

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world!")
 
+
+	// create variable of string named whatTosay and call it with fmt.Println
 	var whatToSay string
+	// create variable int depending on bit computer. Int can be used as standar.
+
+	var i int
 
 	whatToSay = "Let's say hello again, world!"
 
 	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("is is set to", i)
 }
