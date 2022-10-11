d04e0f1862b6c54df6a5d7e7c4becd2cd53e8912
Carlos Garcia <carlos.grh@outlook.com>
2022-10-11 07:01:08 -0500

    create string variable and printline

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
-
+// Print Hello, world! to the cli
 func main() {
 	fmt.Println("Hello, world!")
+
+	var whatToSay string
+
+	whatToSay = "Let's say hello again, world!"
+
+	fmt.Println(whatToSay)
 }
